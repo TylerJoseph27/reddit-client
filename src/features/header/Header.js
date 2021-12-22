6c6e4ff1410492dace1761fad98ae95681703108
TylerJoseph27 <87105915+TylerJoseph27@users.noreply.github.com>
2021-12-21 18:34:56 -0800

    Fix mobile navbar

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -9,36 +9,43 @@ export default function Header() {
   // create mediaQueryList object with matchMedia 
   const mediaQuery = window.matchMedia('only screen and (min-width: 1280px)');
 
-  // toggle menu button
-  const toggleMenu = ({ target }) => {
-    // check if menu button is open
-    if (target.src === (window.location.origin + '/images/openMenu.png')) {
-      // change open menu button to close menu
-      target.src = '/images/closeMenu.png';
-      // open drop-down menu
-      document.querySelector('.navbar').style.opacity = '1';
-      document.querySelector('.navbar').style.transform = 'scaleY(1)';
-    } else if (target.src === (window.location.origin + '/images/closeMenu.png')) {
-      // change close menu button to open menu
-      target.src = '/images/openMenu.png';
-      // close drop-down menu
-      document.querySelector('.navbar').style.opacity = '0';
-      document.querySelector('.navbar').style.transform = 'scaleY(0)';
-    }
-  };
-
+  // toggle navbar on window resize
   const toggleNavbar = event => {
+    // get navbar
+    const navbar = document.querySelector('.navbar');
+
     // check if the media query is true
     if (event.matches) {
       // make navbar visible
-      document.querySelector('.navbar').style.opacity = '1';
-      document.querySelector('.navbar').style.transform = 'scaleY(1)';
+      navbar.style.opacity = '1';
+      navbar.style.transform = 'scaleY(1)';
     } else {
-      // set menu button to open
+      // reset menu button
       document.querySelector('.menu img').src = '/images/openMenu.png';
       // make navbar invisible
-      document.querySelector('.navbar').style.opacity = '0';
-      document.querySelector('.navbar').style.transform = 'scaleY(0)';
+      navbar.style.opacity = '0';
+      navbar.style.transform = 'scaleY(0)';
+    }
+  };
+
+  // toggle menu button on click
+  const toggleMenu = ({ target }) => {
+    // get navbar and menu button
+    const navbar = document.querySelector('.navbar');
+
+    // check if menu button is clicked
+    if (target.src === (window.location.origin + '/images/openMenu.png')) {
+      // toggle menu button
+      target.src = '/images/closeMenu.png';
+      // open drop-down menu
+      navbar.style.opacity = '1';
+      navbar.style.transform = 'scaleY(1)';
+    } else if (target.src === (window.location.origin + '/images/closeMenu.png')) {
+      // reset menu button
+      target.src = '/images/openMenu.png';
+      // close drop-down menu
+      navbar.style.opacity = '0';
+      navbar.style.transform = 'scaleY(0)';
     }
   };
 
